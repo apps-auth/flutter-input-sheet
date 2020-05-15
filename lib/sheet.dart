0c5f38d80a36f6b97e2dc6699a05dab76004bf7a
Adriel Auth <adrielauth3@gmail.com>
2020-05-15 14:40:59 -0300

    fix keyboard bug

--- lib/sheet.dart
+++ lib/sheet.dart
@@ -26,9 +26,7 @@ class Sheet {
     showModalBottomSheet(
       backgroundColor: Colors.white,
       context: _context,
-      enableDrag: true,
-      isDismissible: true,
-      useRootNavigator: true,
+      isScrollControlled: true,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(10),
@@ -36,128 +34,122 @@ class Sheet {
         ),
       ),
       builder: (BuildContext context) {
-        return Scaffold(
-          // use CupertinoPageScaffold for iOS
-          backgroundColor: Colors.transparent,
-          resizeToAvoidBottomInset: true, // important
-          body: SingleChildScrollView(
-            child: Form(
-              child: Container(
-                child: Column(
+        return AnimatedPadding(
+          padding: MediaQuery.of(context).viewInsets,
+          duration: const Duration(milliseconds: 100),
+          curve: Curves.decelerate,
+          child: Wrap(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(0),
+                height: 45,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border(
+                    bottom: BorderSide(color: IpsColors.border, width: 1),
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(0),
-                      height: 45,
-                      width: double.maxFinite,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: Border(
-                          bottom: BorderSide(color: IpsColors.border, width: 1),
+                    RawMaterialButton(
+                      onPressed: () {
+                        Navigator.pop(_context);
+                        ipsInput.onCancel();
+                      },
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            FeatherIcons.x,
+                            size: 21,
+                            color: IpsColors.dark,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            _cancelText,
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: IpsColors.dark,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: SizedBox.shrink(),
+                    ),
+                    Visibility(
+                      visible: showDoneButton == true,
+                      child: RawMaterialButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                          ipsInput.onDone();
+                        },
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              _doneText,
+                              style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 14,
+                                color: IpsColors.dark,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Icon(
+                              FeatherIcons.check,
+                              size: 21,
+                              color: IpsColors.dark,
+                            ),
+                          ],
                         ),
                       ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          RawMaterialButton(
-                            onPressed: () {
-                              Navigator.pop(_context);
-                              ipsInput.onCancel();
-                            },
-                            padding: EdgeInsets.symmetric(horizontal: 10),
-                            child: Row(
-                              children: <Widget>[
-                                Icon(
-                                  FeatherIcons.x,
-                                  size: 21,
-                                  color: IpsColors.dark,
-                                ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  _cancelText,
-                                  style: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 14,
-                                    color: IpsColors.dark,
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            child: SizedBox.shrink(),
-                          ),
-                          Visibility(
-                            visible: showDoneButton == true,
-                            child: RawMaterialButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                                ipsInput.onDone();
-                              },
-                              padding: EdgeInsets.symmetric(horizontal: 10),
-                              child: Row(
-                                children: <Widget>[
-                                  Text(
-                                    _doneText,
-                                    style: TextStyle(
-                                      fontFamily: 'Montserrat',
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 14,
-                                      color: IpsColors.dark,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Icon(
-                                    FeatherIcons.check,
-                                    size: 21,
-                                    color: IpsColors.dark,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: paddingVertical,
-                      ),
-                      child: Column(
-                        children: <Widget>[
-                          Visibility(
-                            visible: _label != null,
-                            child: Padding(
-                              padding: EdgeInsets.only(bottom: 5),
-                              child: Text(
-                                _label ?? "",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontFamily: 'Montserrat',
-                                  fontSize: 16,
-                                  color: IpsColors.gray,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                          ipsInput
-                        ],
-                      ),
-                    ),
-                    // SizedBox(
-                    //   height: MediaQuery.of(_context).viewInsets.bottom != 0
-                    //       ? MediaQuery.of(_context).viewInsets.bottom
-                    //       : 0,
-                    // ),
                   ],
                 ),
               ),
-            ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: paddingVertical,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Visibility(
+                      visible: _label != null,
+                      child: Padding(
+                        padding: EdgeInsets.only(bottom: 5),
+                        child: Text(
+                          _label ?? "",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 16,
+                            color: IpsColors.gray,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    ipsInput
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(_context).viewInsets.bottom != 0
+                    ? MediaQuery.of(_context).viewInsets.bottom
+                    : 0,
+              ),
+            ],
           ),
         );
       },
